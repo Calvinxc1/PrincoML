717ea1eec76163936af991151e9b916d4a549db6
Michael Pilosov <consistentbayes@gmail.com>
2019-04-11 19:24:06 +0000

    lowercase name

--- PrincoML/utils/learn_modules/learn_rates/FlatLearnRate.py
+++
@@ -1,6 +0,0 @@
-from PrincoML.utils.learn_modules.learn_rates.RootLearnRate import RootLearnRate as Root
-
-class FlatLearnRate(Root):
-    @property
-    def learn_rate(self):
-        return self.seed_learn
